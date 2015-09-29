a58c2215a304a4d6b54a65e74764286aedb42514
unknown <cleo.chig@gmail.com>
2015-09-29 15:52:23 -0300

    alterando informações

--- pestControl/www/js/app.js
+++ pestControl/www/js/app.js
@@ -25,6 +25,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
+    .state('app.login', {
+      url: '/login',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/login.html'
+        }
+      }
+    })
+
     .state('app', {
     url: '/app',
     abstract: true,
@@ -69,5 +78,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/login');
 });
